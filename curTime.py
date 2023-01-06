adfcfa3ac2972f3b73fff33f82178be7d6209710
Aditya Singh <2710adityasingh@gmail.com>
2023-01-06 23:51:47 +0530

    Deleted temp file

--- curTime.py
+++
@@ -1,5 +0,0 @@
-title_slug = "01-matrix"
-problem_link = f"https://leetcode.com/problems/{title_slug}"
-hyperlink = f'<a href="{problem_link}">{title_slug}</a>'
-body = f"Hi there,\nYou haven\'t submitted the today's LeetCode Challenge. Please submit the solution asap.\nProblem Link: {hyperlink}"
-print(body)
